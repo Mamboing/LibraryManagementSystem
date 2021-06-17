7e89541deaeaebe541908a5079e007a6c65df59a
mamboing <1302855173@qq.com>
2021-06-18 01:48:43 +0800

    Show Login result in Toast

--- app/src/main/java/com/example/librarymanagementsystem/data/LoginDataSource.kt
+++ app/src/main/java/com/example/librarymanagementsystem/data/LoginDataSource.kt
@@ -1,12 +1,12 @@
 package com.example.librarymanagementsystem.data
 
+import android.os.SystemClock
 import com.example.librarymanagementsystem.data.model.LoggedInUser
 import com.example.librarymanagementsystem.util.HttpUtil
 import com.example.librarymanagementsystem.util.LogUtil
 import okhttp3.Call
 import okhttp3.Callback
 import okhttp3.Response
-import org.json.JSONArray
 import java.io.IOException
 
 
@@ -16,6 +16,7 @@ import java.io.IOException
 class LoginDataSource {
 
     fun login(username: String, password: String): Result<LoggedInUser> {
+        var dataCount = "E"
         val address: String = HttpUtil.LocalAddress + "/login?userid=" + username + "&password=" + password
         HttpUtil.getHttp(address, object : Callback{
             override fun onFailure(call: Call, e: IOException) {
@@ -25,16 +26,23 @@ class LoginDataSource {
 
             override fun onResponse(call: Call, response: Response) {
                 val responseData = response.body?.string()!!
+                val responseDataCount = responseData.substring(1, 2)
+                dataCount = responseDataCount
+
+                LogUtil.d("LoginResponseCount", responseDataCount)
                 LogUtil.d("LoginResponse", responseData)
             }
         })
 
-        try {
-            // TODO: handle loggedInUser authentication
-            val fakeUser = LoggedInUser(java.util.UUID.randomUUID().toString(), "Jane Doe")
-            return Result.Success(fakeUser)
-        } catch (e: Throwable) {
-            return Result.Error(IOException("Error logging in", e))
+        // 防止因异步请求造成的dataCount数据冲突
+        SystemClock.sleep(1000)
+
+        LogUtil.d("ResponseCount", dataCount)
+        return if (dataCount != "E") {
+            val fakeUser = LoggedInUser(java.util.UUID.randomUUID().toString(), username)
+            Result.Success(fakeUser)
+        } else {
+            Result.Error("Error logging in")
         }
     }
 
